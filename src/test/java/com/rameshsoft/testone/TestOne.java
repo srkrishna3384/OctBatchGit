54fdca3409805fddbfc0cc4302ef7e75c4c31b16
srkrishna3384 <srkrishna3384@gmail.com>
2020-10-31 18:20:40 +0530

    Added TestOne.java file

---
+++ src/test/java/com/rameshsoft/testone/TestOne.java
@@ -0,0 +1,23 @@
+package com.rameshsoft.testone;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestOne {
+	
+	@Test
+	public void demoOne() {
+		//System.s
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Launch the Chrome Browser...");
+		driver.get("https://www.rameshsoft.com");
+		System.out.println("Navigate to www.rameshsoft.com");
+		driver.close();
+		System.out.println("Close the Browser.........");
+	}
+
+}
